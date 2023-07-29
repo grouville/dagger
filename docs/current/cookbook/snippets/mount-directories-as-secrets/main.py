16cd31daab727b386a726c1e6b189f53b17cd537
Helder Correia <174525+helderco@users.noreply.github.com>
2023-07-29 22:33:06 +0000

    Simplify mounting a secret directory cookbook
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>

--- docs/current/cookbook/snippets/mount-directories-as-secrets/main.py
+++ docs/current/cookbook/snippets/mount-directories-as-secrets/main.py
@@ -1,62 +1,44 @@
 import os
 import sys
-from pathlib import Path
 
 import anyio
 
 import dagger
 
+GPG_KEY = os.environ.get("GPG_KEY", "public")
+
 
 async def main():
     async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
-        gpg_files = [
-            "/home/USER/.gnupg/pubring.kbx",
-            "/home/USER/.gnupg/trustdb.gpg",
-            "/home/USER/.gnupg/public.key",
-        ]
-
-        source_dir = "/home/USER/.gnupg/private-keys-v1.d/"
-        target_dir = "/root/.gnupg/private-keys-v1.d/"
-
-        ctr = client.container().from_("alpine:3.17")
-
-        for file_path in gpg_files:
-            secret = client.host().set_secret_file(Path.name(file_path), file_path)
-            ctr = ctr.with_mounted_secret(
-                f"/root/.gnupg/{Path.name(file_path)}", secret
-            )
-
-        ctr = ctr.with_(mount_directory_as_secret(client, source_dir, target_dir))
-
-        binary_name = "binary-name"
-        ctr = (
-            ctr.with_exec(["apk", "add", "--no-cache", "gnupg"])
-            .with_exec(["chmod", "700", "/root/.gnupg"])
-            .with_exec(["chmod", "700", "/root/.gnupg/private-keys-v1.d"])
-            .with_exec(["gpg", "--import", "/root/.gnupg/public.key"])
-            .with_exec(
-                ["gpg", "--import", "/root/.gnupg/private-keys-v1.d/private.key"]
-            )
+        await (
+            client.container()
+            .from_("alpine:3.17")
+            .with_exec(["apk", "add", "--no-cache", "gnupg"])
+            .with_(await mounted_secret_directory(client, "/root/.gnupg", "~/.gnupg"))
             .with_workdir("/root")
-            .with_exec(["gpg", "--detach-sign", "--armor", binary_name])
-            .with_exec(["ls", "-l", f"{binary_name}.asc"])
+            .with_mounted_file("myapp", client.host().file("myapp"))
+            .with_exec(["gpg", "--detach-sign", "--armor", "-u", GPG_KEY, "myapp"])
+            .export("myapp.asc")
         )
 
-        print(await ctr.stdout())
 
+async def mounted_secret_directory(client: dagger.Client, target_path: str, source_path: str):
+    target = anyio.Path(target_path)
+    base = await anyio.Path(source_path).expanduser()
+    files = [path async for path in base.rglob("*") if await path.is_file()]
 
-def mount_directory_as_secret(client, source_dir, target_dir):
-    def mount_directory_as_secret_inner(ctr: dagger.Container):
-        for root, _dirs, files in os.walk(source_dir):
-            for file in files:
-                full_path = Path(root) / file
-                target_path = Path(target_dir) / Path(full_path).relative_to(source_dir)
+    def _mounted_secret_directory(ctr: dagger.Container) -> dagger.Container:
+        for path in files:
+            relative = path.relative_to(base)
+            secret = client.host().set_secret_file(str(relative), str(path))
+            ctr = ctr.with_mounted_secret(str(target / relative), secret)
 
-                secret = client.host().set_secret_file(Path.name(full_path), full_path)
-                ctr = ctr.with_mounted_secret(target_path, secret)
-        return ctr
+        # Fix directory permissions.
+        return ctr.with_exec(
+            ["sh", "-c", f"find {target} -type d -exec chmod 700 {{}} \\;"]
+        )
 
-    return mount_directory_as_secret_inner
+    return _mounted_secret_directory
 
 
 anyio.run(main)
